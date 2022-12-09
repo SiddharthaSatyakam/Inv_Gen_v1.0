af3630c4fb6d8e0717d13038ed55e5dbd15e55a2
SiddharthaSatyakam <siddharthsatyakam870@gmail.com>
2022-12-09 17:39:03 +0530

    Sid final v1.6

--- invoice.js
+++ invoice.js
@@ -85,9 +85,6 @@ $(document).ready(function () {
 
     document.getElementById("print").addEventListener('click',print);
     function print(){
-        document.getElementById("addRow").style.visibility='hidden';
-        document.getElementById("remRow").style.visibility='hidden';
-        document.getElementById("btn").style.visibility='hidden';
         window.print();
     }
 
